9a2741693dc3acd8777ba54ed5137f40c2a91314
latawan <latawan@sheridancollege.ca>
2020-02-18 20:51:45 -0500

    3rd

--- tester/src/tester/Tester.java
+++ tester/src/tester/Tester.java
@@ -8,11 +8,24 @@ package tester;
  */
 public class Tester {
 
-    /**
+    /**asdasdasdasdasdasdasdasdsaasassassa
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("testertesssss");
     }
+    public static void main(String[] args) {
+        System.out.println("testertesssss");
+    }
+    public static void main(String[] args) {
+        System.out.println("testertesssss");
+    }
+    public static void main(String[] args) {
+        System.out.println("testertesssss");
+    }
+    public static void main(String[] args) {
+        System.out.println("testertesssss");
+    }
+    
     
 }
