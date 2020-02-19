0fb899ce95ad81a2497f2470ec5d21645c4fd52c
latawan <59924389+latawan@users.noreply.github.com>
2020-02-18 20:53:22 -0500

    Update Tester.java
    lol

--- tester/src/tester/Tester.java
+++ tester/src/tester/Tester.java
@@ -12,19 +12,21 @@ public class Tester {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("testertesssss");
-    }
-    public static void main(String[] args) {
-        System.out.println("testertesssss");
-    }
-    public static void main(String[] args) {
-        System.out.println("testertesssss");
-    }
-    public static void main(String[] args) {
-        System.out.println("testertesssss");
-    }
-    public static void main(String[] args) {
-        System.out.println("testertesssss");
+       
+         System.out.println("testertesssss");
+         System.out.println("testertesssss");
+         System.out.println("testertesssss");
+         System.out.println("testertesssss");
+         System.out.println("testertesssss");
+         System.out.println("testertesssss");
+         System.out.println("testertesssss");
+         System.out.println("testertesssss");
+         System.out.println("testertesssss");
+         System.out.println("testertesssss");
+         System.out.println("testertesssss");
+         System.out.println("testertesssss");
+        
+    
     }
     
     
